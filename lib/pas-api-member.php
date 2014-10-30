330a85637e0c2836805748575d98689509e8a673
Robert Harjes <harje005@d.umn.edu>
2014-10-30 11:47:24 -0700

    Efficient member/tracker stats by day

--- lib/pas-api-member.php
+++ lib/pas-api-member.php
@@ -61,11 +61,11 @@ class PAS_Member extends PAS_API {
     }
     
     public function getMemberTickets() { 
-	    $get = $this->sendRequest('/publisher_members/'.$this->member_id.'/tickets.xml', 'GET');
-	    foreach($get->ticket as $ticket) { 
+      $get = $this->sendRequest('/publisher_members/'.$this->member_id.'/tickets.xml', 'GET');
+      foreach($get->ticket as $ticket) { 
             $tickets[] = new PAS_Ticket($this->member_id, $ticket->id);
         }
-	    return $tickets;
+      return $tickets;
     }
     
     public function addTracker($tracker_identifier, $offer_id) { 
@@ -73,52 +73,73 @@ class PAS_Member extends PAS_API {
                         <identifier>'.addslashes($tracker_identifier).'</identifier>
                         <website_offer_id>'.$offer_id.'</website_offer_id>
                     </member_tracker>';
-	    $add_tracker = $this->sendRequest('/publisher_members/'.$this->member_id.'/publisher_member_trackers.xml', 'POST', $payload);
-	    return $add_tracker;
+      $add_tracker = $this->sendRequest('/publisher_members/'.$this->member_id.'/publisher_member_trackers.xml', 'POST', $payload);
+      return $add_tracker;
     }
     
     // Dates should be in this format: 'YYYY-MM-DD'
+    // returns stats container with individual target_date stats within each tracker
+    // e.g. $statistics->mgr = total mgr for period
+    // e.g. $statistics->member_trackers->member_tracker[0]->mgr = total mgr for tracker for period
+    // e.g. $statistics->member_trackers->member_tracker[0]->stats->target_date[0]->mgr = total mgr for tracker on $start_date
     public function getMemberStats($start_date=null, $end_date=null) { 
         if($start_date != null && $end_date != null) {
             $add_params = '&start_date='.$start_date.'&end_date='.$end_date;
         } else { $add_params = ''; }
         
-	    $stats = $this->sendRequest('/publisher_members/'.$this->member_id.'/stats.xml', 'GET', null, $add_params);
-	    return $stats;
+      $stats = $this->sendRequest('/publisher_members/'.$this->member_id.'/stats.xml', 'GET', null, $add_params);
+      return $stats;
     }
 
     // Dates should be in this format: 'YYYY-MM-DD'.  
-/////     NOTE: This method is currently limited to 1 month.  Any longer will likely timeout!
-	public function getMemberStatsByDay($start_date, $end_date) { 
-		// Validate Dates
-		$ts_start = PAS_API::date2timestamp($start_date);
-		$ts_end = PAS_API::date2timestamp($end_date);
-		if(date('m', $ts_start) != date('m', $ts_end) || date('Y', $ts_start) != date('Y', $ts_end)) { 
-			return false; // Our dates eclipsed 1 month or 1 year --> Would TIMEOUT if run, send FALSE!
-		}
-		
-		// Are we working within this month?
-		if(date('Ym') == date('Ym', $ts_start)) { 
-			// We are --> We ONLY need to request up to today (yesterday, really, but we'll do today for completeness)
-			$ts_end = PAS_API::date2timestamp(date('Y-m-d'));
-		}
-		
-		// Now, lets go ahead & loop through each day!
-		$current_time = $ts_start;
-		while($current_time <= $ts_end) {
-			$add_params = '&start_date='.date('Y-m-d',$current_time).'&end_date='.date('Y-m-d',$current_time);
-	    	$stats = $this->sendRequest('/publisher_members/'.$this->member_id.'/stats.xml', 'GET', null, $add_params);
-
-			// Build an array of Daily Stats for later return.
-			$daily_stats_array[date('Y-m-d',$current_time)] = $stats;
-			
-			// On the next iteration, get `tomorrow`!
-			$current_time += 86400;
-		}
-
-		// Return the Daily Stats!
-	    return $daily_stats_array;
-	}
+    // Returns stats container in associative array of dates
+    // This function now just rebuilds the (now updated) API response --
+    //  (efficient stats by day -- 1 API call) for legacy applications
+  public function getMemberStatsByDay($start_date = null, $end_date = null) { 
+    $statistics   = $this->getMemberStats($start_date, $end_date);
+    
+    date_default_timezone_set('America/Los_Angeles');
+    
+    $results      = array();
+    $target_date  = strtotime($statistics['start_date']);
+    $last_day     = strtotime($statistics['end_date']);
+    
+    while ($target_date <= $last_day) {      
+      $target_date_string     = date('Y-m-d', $target_date);
+      $total_mgr_for_day      = 0;
+      $total_rakeback_for_day = 0;
+      
+      $stat = new SimpleXMLElement('<statistics></statistics>');
+      $stat['start_date'] = $target_date_string;
+      $stat['end_date']   = $target_date_string;
+      $stat->member_id    = $statistics->member_id;
+      
+      foreach($statistics->member_trackers->member_tracker as $member_tracker) {
+        $t                = $stat->addChild('member_trackers');
+        $t['type']        = 'array';
+        $t->id            = $member_tracker->id;
+        $t->identifier    = $member_tracker->identifier;
+        $t->poker_room_id = $member_tracker->poker_room_id;
+        $t->poker_room    = $member_tracker->poker_room;
+        
+        foreach($member_tracker->stats->target_date as $date) {
+          if ($date['value'] == $target_date_string) {
+            $total_mgr_for_day      += ($t->mgr = $date->mgr);
+            $total_rakeback_for_day += ($t->rakeback = $date->rakeback);
+          }
+        }
+      }
+      
+      $stat->mgr      = $total_mgr_for_day;
+      $stat->rakeback = $total_rakeback_for_day;
+      
+      $results[$target_date_string] = $stat;
+      
+      $target_date = strtotime("+1 day", $target_date);
+    }
+    
+    return $results;
+  }
     
     // Dates should be in this format: 'YYYY-MM-DD'
     public function getMemberRAFStats($start_date=null, $end_date=null) { 
@@ -126,8 +147,8 @@ class PAS_Member extends PAS_API {
             $add_params = '&start_date='.$start_date.'&end_date='.$end_date;
         } else { $add_params = ''; }
         
-	    $stats = $this->sendRequest('/publisher_members/'.$this->member_id.'/referrals.xml', 'GET', null, $add_params);
-	    return $stats;
+      $stats = $this->sendRequest('/publisher_members/'.$this->member_id.'/referrals.xml', 'GET', null, $add_params);
+      return $stats;
     }
     
     public function getMemberPayments($start_date=null, $end_date=null, $page=1) { 
@@ -178,31 +199,31 @@ class PAS_Member extends PAS_API {
         return $cashout_methods;
     }
     
-	public function getRemoteAuthToken() { 
-	    $get = $this->sendRequest('/remote_auth.xml', 'POST', '<member_id>'.$this->member_id.'</member_id>');
-	    return (string) $get[0];
-	}
+  public function getRemoteAuthToken() { 
+      $get = $this->sendRequest('/remote_auth.xml', 'POST', '<member_id>'.$this->member_id.'</member_id>');
+      return (string) $get[0];
+  }
     
     private function getMemberData() { 
-	    $this->xml_obj = $this->sendRequest('/publisher_members/'.$this->member_id.'.xml');
+      $this->xml_obj = $this->sendRequest('/publisher_members/'.$this->member_id.'.xml');
         if(parent::hasErrors($this->xml_obj)) { 
             return false;
         } return true;
-	}
-	
-	private function updateMember() { 
-	    $update = $this->sendRequest('/publisher_members/'.$this->member_id.'.xml', 'PUT', $this->asXML());
+  }
+  
+  private function updateMember() { 
+      $update = $this->sendRequest('/publisher_members/'.$this->member_id.'.xml', 'PUT', $this->asXML());
         if(parent::hasErrors($update)) { 
             return false;
         } return true;
-	}
-	
-	private function createMember() { 
-	    $create = $this->sendRequest('/publisher_members.xml', 'POST', $this->asXML());
-	    if(parent::hasErrors($create)) { 
+  }
+  
+  private function createMember() { 
+      $create = $this->sendRequest('/publisher_members.xml', 'POST', $this->asXML());
+      if(parent::hasErrors($create)) { 
             return false;
         } return true;
-	}    
+  }    
 }
 
 ?>
